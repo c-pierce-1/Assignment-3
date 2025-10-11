c02b7497ebcd47a3d911890c73149e61ac169b00
c-pierce-1 <c.pierce27284@gmail.com>
2025-10-10 21:23:26 -0400

    Completed AnimalService class

--- src/main/java/com/example/demo/AnimalRepository.java
+++ src/main/java/com/example/demo/AnimalRepository.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface AnimalRepository extends JpaRepository<Animal, Long> {
     List<Animal> getAnimalsByType(String animalType);
+
+    @Query(value = "SELECT * FROM animals a WHERE a.age >= ?1", nativeQuery = true)
     List<Animal> getOlderAnimals(double age);
+
+    @Query(value = "SELECT * FROM animals a WHERE a.name ILIKE %?1%", nativeQuery = true)
     List<Animal> getAnimalsByName(String name);
 }
---
+++ src/main/java/com/example/demo/AnimalService.java
@@ -0,0 +1,69 @@
+import java.io.IOException;
+import java.io.File;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+@Service
+public class AnimalService{
+    
+    @Autowired
+    private AnimalRepository animalRepository;
+
+    public Object getAllAnimals(){
+        return animalRepository.findAll();
+    }
+
+    public Animal getAnimalById(Long animalId){
+        return animalRepository.findById(animalId).orElse(null);
+    }
+
+    public Object getAnimalsByName(String name){
+        return animalRepository.getAnimalsByName(name);
+    }
+
+    public Object getAnimalsByType(String animalType){
+        return animalRepository.getAnimalsByType(animalType);
+    }
+
+    public Object getOlderAnimals(double age){
+        return animalRepository.getOlderAnimals(age);
+    }
+
+    public Animal addAnimal(Animal animal){
+        return animalRepository.save(animal);
+    }
+
+    public Animal updateAnimal(Long animalId, Animal animal){
+        animal.setAnimalId(animalId);
+        return animalRepository.save(animal);
+    }
+
+    public void deleteAnimal(Long animalId){
+        animalRepository.deleteById(animalId);
+    }
+
+    public String writeJson(Animal animal){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            objectMapper.writeValue(new File("animals.json"), animal);
+            return "Animal written to JSON file successfully.";
+        } catch (IOException e){
+            e.printStackTrace();
+            return "Error writing animal to JSON file.";
+        }
+    }
+
+    public Object readJson(){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try{
+            return objectMapper.readValue(new File("animals.json"), Animal.class);
+        } catch (IOException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
