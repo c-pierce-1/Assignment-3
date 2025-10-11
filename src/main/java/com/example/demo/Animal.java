d18c091170d5aed01fb5dd154578a3d546928392
c-pierce-1 <c.pierce27284@gmail.com>
2025-10-10 20:09:43 -0400

    Completed Animal class

---
+++ src/main/java/com/example/demo/Animal.java
@@ -0,0 +1,80 @@
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "animals")
+public class Animal{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long animalId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description; 
+    private double age;
+    private String animalType;
+
+    public Animal(){
+    }
+
+    public Animal(Long animalId, String name, String description, double age, String animalType){
+        this.animalId = animalId;
+        this.name = name;
+        this.description = description;
+        this.age = age;
+        this.animalType = animalType;
+    }
+
+    public Animal( String name, String description, double age, String animalType){
+        this.name = name;
+        this.description = description;
+        this.age = age;
+        this.animalType = animalType;
+    }
+
+    public Long getAnimalId(){
+        return animalId;
+    }
+
+    public void setAnimalId(Long id){
+        this.animalId = id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getDescription(){
+        return description;
+    }
+
+    public void setDescription(String description){
+        this.description = description;
+    }
+
+    public double getAge(){
+        return age;
+    }
+
+    public void setAge(double age){
+        this.age = age;
+    }
+
+    public String getAnimalType(){
+        return animalType;
+    }
+
+    public void setAnimalType(String animalType){
+        this.animalType = animalType;
+    }
+}
